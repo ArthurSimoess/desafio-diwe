859bc4b30f48987c1fe7acc33de36d56af7f2d41
Arthur Simões <arthursimoes155@gmail.com>
2022-05-28 16:11:30 -0300

    feat: implements the filterResults function, which sorts contacts according to id, name, cellphone or email

--- src/components/btnHeaderTable/BtnHeaderTable.jsx
+++ src/components/btnHeaderTable/BtnHeaderTable.jsx
@@ -1,23 +1,55 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 import PropTypes from 'prop-types';
-import React from 'react';
-import arrowDown from '../../assets/images/chevron-down.svg';
+import React, { useContext, useState } from 'react';
+import arrowDown from '../../assets/images/arrow-down.svg';
+import MyContext from '../../context/MyContext';
 import './btnTableStyle.scss';
 
-function BtnHeaderTable({ title }) {
+function BtnHeaderTable({ title, keyObj }) {
+  const { filter, setFilter } = useContext(MyContext);
+  const [arrow, setArrow] = useState('down');
+
+  const resetObj = {
+    id: false,
+    name: false,
+    mobile: false,
+    email: false,
+  };
+
+  function handleClick() {
+    const titles = ['id', 'name', 'mobile', 'email'];
+    const [select] = titles.filter((ele) => (filter[ele] === true));
+    if (select && select === keyObj) {
+      setFilter({ ...resetObj });
+      setArrow('down');
+    } else {
+      titles.forEach((item) => {
+        if (keyObj === item) {
+          setArrow('up');
+          setFilter({
+            ...resetObj,
+            [item]: true,
+          });
+        }
+      });
+    }
+  }
+
   return (
     <button
       type="button"
+      onClick={handleClick}
       className="table-btn"
     >
       <p>{title}</p>
-      <img src={arrowDown} alt="Seta apontando para baixo" />
+      <img src={arrowDown} alt="Seta apontando para baixo" className={arrow} />
     </button>
   );
 }
 
 BtnHeaderTable.propTypes = {
   title: PropTypes.string.isRequired,
+  keyObj: PropTypes.string.isRequired,
 };
 
 export default BtnHeaderTable;
